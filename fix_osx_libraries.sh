ac45d737b353559aa25414f25901dd2c6c128bfe
Steven Boyd <skboyd@ucalgary.ca>
2021-06-09 20:15:02 -0600

    Updated how to build archive

--- fix_osx_libraries.sh
+++ fix_osx_libraries.sh
@@ -2,9 +2,13 @@
 #
 # This script must be run from directory containing the library or executable to modify.
 #
-# it is typically run like this:
+# It is typically run like this:
 #
-# ~/code/n88ParaViewPlugins/5.1.2/n88ParaViewPlugins/fix_osx_libraries.sh /Volumes/CaseSensitive/build/ParaViewSuperbuild/v5.1.2/build libImageGaussianSmooth.dylib
+# cd /Volumes/CaseSensitive/build/n88ParaViewPlugins/v5.9.1/n88ParaViewPlugins-build/lib/paraview-5.9/plugins/ImageGaussianSmooth
+# /Volumes/CaseSensitive/build/n88ParaViewPlugins/v5.9.1/n88ParaViewPlugins/fix_osx_libraries.sh \
+#	  /Volumes/CaseSensitive/build/ParaViewSuperbuild/v5.9.1/build \
+#   ImageGaussianSmooth.so
+#
 
 paraviewBuildPath="$1"
 libpath="$2"
@@ -37,14 +41,16 @@ do
 done
 
 # Generic dylibs
-replace_path "$paraviewBuildPath"/paraview/src/paraview-build/lib "@executable_path/../Libraries/"
+replace_path "$paraviewBuildPath"/superbuild/paraview/build/lib "@executable_path/../Libraries/"
 replace_path "$paraviewBuildPath"/install/lib "@executable_path/../Libraries/"
 replace_path "@rpath" "@executable_path/../Libraries/"
 # replace_path "$PWD" "@loader_path/"
 
 # tbb
-replace_path "$paraviewBuildPath"/tbb/src/tbb/lib "@executable_path/../Libraries/"
+replace_path "$paraviewBuildPath"/install/lib "@executable_path/../Libraries/"
 
 # HDF5 libraries
-install_name_tool -change libhdf5_hl.8.0.2.dylib @executable_path/../Libraries/libhdf5_hl.8.0.2.dylib "$libpath"
-install_name_tool -change libhdf5.8.0.2.dylib @executable_path/../Libraries/libhdf5.8.0.2.dylib "$libpath"
+install_name_tool -change libnetcdf.15.dylib @executable_path/../Libraries/libnetcdf.15.dylib "$libpath"
+install_name_tool -change libhdf5_hl.200.dylib @executable_path/../Libraries/libhdf5_hl.200.dylib "$libpath"
+install_name_tool -change libhdf5.200.dylib @executable_path/../Libraries/libhdf5.200.dylib "$libpath"
+
